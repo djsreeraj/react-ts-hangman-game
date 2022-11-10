548b4c1d39cde85bfafd2d0908af93f535693b2c
djsreeraj <sreeraj011m@gmail.com>
2022-11-10 05:44:34 +0530

    added words, states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import words from './wordList.json'
 function App() {
+
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  
 
   return (
     <div className="App">
-      Welcome to Hangman App
+      Welcome to Hangman App - {wordToGuess}
     </div>
   )
 }
